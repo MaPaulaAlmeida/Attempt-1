a74fc836a307a79cb9929b79ddf95aeaac7e66ea
MaPaulaAlmeida <169.mariapaula@gmail.com>
2024-12-02 02:43:52 +0000

    Version 5 - 70%
    Still trying to fix some bugs. Mainly details on the navbar and animations.

--- app2.js
+++ app2.js
@@ -9,8 +9,8 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 5000;
-let timeAutoNext = 10000;
+let timeRunning = 4000;
+let timeAutoNext = 8000;
 
 nextDom.onclick = function(){
     showSlider('next');    
